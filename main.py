a6f42c18bc8e2481ae0ae0a752aa19cd10fbb600
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-08-03 11:20:03 +0900

    Shorten the code in preparation for the merging with childfirst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.properties import StringProperty
-
+from kivy.uix.boxlayout import BoxLayout
 from os.path import join, dirname
 from os import environ
 import gettext
@@ -14,45 +14,38 @@ export CHILD_FIRST_LANG='ja'
 export CHILD_FIRST_LANG='en'
 '''
 
+
 class _(str):
-    observers = []
     lang = None
+
     def __new__(cls, s, *args, **kwargs):
         if _.lang is None:
             try:
                 _.switch_lang(environ['CHILD_FIRST_LANG'])
             except KeyError:
                 _.switch_lang('en')
-        s = _.translate(s, *args, **kwargs)
-        return s
-        # The original call, which cannot deal with unicode
-        # return super(_, cls).__new__(cls, s)
+        return _.translate(s, *args, **kwargs)
 
     @staticmethod
     def translate(s, *args, **kwargs):
         return _.lang(s).format(args, kwargs)
 
     @staticmethod
-    def bind(**kwargs):
-        _.observers.append(kwargs['_'])
-
-    @staticmethod
     def switch_lang(lang):
         # get the right locales directory, and instanciate a gettext
         locale_dir = join(dirname(__file__), 'data', 'locales')
         locales = gettext.translation('langapp', locale_dir,
-            languages=[lang])
+                                      languages=[lang])
         _.lang = locales.ugettext
-        # update all the kv rules attached to this text
-        for callback in _.observers:
-            callback()
+
 
 class LangApp(App):
 
     lang = StringProperty('en')
+
     def on_lang(self, instance, lang):
         environ['CHILD_FIRST_LANG'] = lang
         _.switch_lang(lang)
-        
+
 
 LangApp().run()
